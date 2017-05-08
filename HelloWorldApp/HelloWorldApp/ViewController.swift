762d69ec30a8ab7c1157c77ceb699068f7cee2d3
harinirao <srao.harini@gmail.com>
2017-05-08 22:41:35 +1000

    few cool buttons

---
+++ HelloWorldApp/HelloWorldApp/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  HelloWorldApp
+//
+//  Created by Harini Rao on 1/5/17.
+//  Copyright © 2017 harini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func showMessage() {
+        let alertController = UIAlertController(title: "Welcome to my first app", message: "Hello World", preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+
+
+}
+
